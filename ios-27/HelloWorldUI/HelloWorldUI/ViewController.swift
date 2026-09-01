2713faf2b84908bf1a3496cf4730730f1c9004dc
Facultad de Contaduría y Administración <facultaddecontaduriayadministracion@MacBook-15.local>
2026-09-01 14:58:48 -0600

    p

---
+++ ios-27/HelloWorldUI/HelloWorldUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorldUI
+//
+//  Created by Facultad de Contaduría y Administración on 01/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
